44690f8dfb508f43f4b4deca1dc469c015020cac
karoly.bodei <karoly.bodei@trendency.hu>
2024-09-29 17:24:42 +0200

    feat: Add config management

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+	"strings"
+)
 
+var defaults = map[string]interface{}{
+	"debug": false,
+}
+
+func main() {
+	v := viper.GetViper()
+
+	err := initializeConfig(v)
+
+	if err != nil {
+		log.Fatalln("Failed to initialize config: " + err.Error())
+	}
+
+	println(fmt.Sprintf("Debug: %s", v.GetBool("debug")))
+}
+
+func initializeConfig(v *viper.Viper) error {
+	v.SetEnvPrefix("video_storybook")
+
+	for key, value := range defaults {
+		v.SetDefault(key, value)
+	}
+
+	replacer := strings.NewReplacer(".", "_")
+	v.SetEnvKeyReplacer(replacer)
+
+	v.SetConfigName("config")
+	v.SetConfigType("yaml")
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+
+	return v.ReadInConfig()
 }
